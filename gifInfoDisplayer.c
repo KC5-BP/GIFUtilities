75bcd8e1a27fcf806c3e56bf6b204fd1753b5b27
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2023-07-23 13:23:36 +0200

    Add displayer module

---
+++ gifInfoDisplayer.c
@@ -0,0 +1,84 @@
+/*..--------------------------------------------------------------------------.
+../ .------------------------------------------------------------------------. \
+./´/
+|x| __--""'\
+|x|  ,__  -'""`;
+|x| /   \  /"'  \
+|x|   __// \-"-_/
+|x| ´"   \  |           > Module: gifDecoder
+|x| \     |  \  _.-"',
+|x| "^,-´\/\  '" ,--. \
+|x|  \|\| | | , /    | |
+|x|     '`'\|._ |   / /
+|x|         '\),/  / |          > Creation: July 2023
+|x|           |/.-"_/           > By: KC5-BP
+|x| .__---+-_/'|--"
+|x|         _| |_--,            > Description :
+|x|        ',/ |   /                >   Give the ability to use GIF's decoder
+|x|        /|| |  /                 >   functions from associated 'C' file.
+|x|     |\| |/ |- |
+|x| .-,-/ | /  '/-"
+|x| -\|/-/\/ ^,'|
+|x| _-     |  |/
+|x|  .  --"  /
+|x| /--__,.-/
+.\`\_________________________________________________________________________/´/
+..`___________________________________________________________________________´
+===============================================================================>
+============================================================================= */
+
+#include "gifInfoDisplayer.h"
+#include <stdio.h>
+#include "gifDecoder.h"
+
+/******************************************************************************/
+void gifPrintSignature(char *fmt, struct gifFile *gf) {
+	printf(fmt, gf->header);
+}
+
+/******************************************************************************/
+void gifPrintLogicalScreenDescriptor(struct gifFile *gf, int printGct) {
+	printf("Logical dimension: %dx%d\n", 	gf->lsd.logicDim.width,	\
+											gf->lsd.logicDim.height);
+    printf("Global Color Table infos: %#0x\n", gf->lsd.gctInfos);
+    printf("'-> Bit depth: %d\n", gf->lsd.bitDepth);
+    printf("'-> Presence of a GCT: %s\n", gf->lsd.hasGct ? "YES" : "NO");
+    printf("Pixel aspect ratio: %d\n", gf->lsd.pxAspectRatio);
+
+	if (printGct) {
+		if ( (! gf->lsd.hasGct) || (! gf->gct.nPal) || (! gf->gct.palette) ) {
+			printf("Could not print GCT because file doesn't have one.\n");
+			return;
+		}
+
+        printf("%s--- Global Color Table ---%s\n", LIGHT_CYAN, NC);
+		printf("    -R-    -G-    -B-\n");
+        for (int i = 0; i < gf->gct.nPal; ++i)
+            printf("    x%02X    x%02X    x%02X\n",	gf->gct.palette[i].r, \
+												gf->gct.palette[i].g, \
+												gf->gct.palette[i].b);
+	}
+}
+
+/******************************************************************************/
+void gifPrintGce(struct gifFile *gf) {
+	printf("Extension code: %s\n",  (gf->gce.extCode == GIF_PIC_EXT_CODE) ?   \
+									(GIF_PIC_EXT_STRING) :         			  \
+                                    ((gf->gce.extCode == GIF_ANIM_EXT_CODE) ?	\
+                                        (GIF_ANIM_EXT_STRING) :          \
+                                        ("Unknown extension code")));
+	printf("Amount of GCE Datas: %d\n", gf->gce.nGceDatas);
+
+	if (gf->gce.extCode == GIF_PIC_EXT_CODE) {
+    	printf("Has transparency: %s\n", gf->gce.gceSpecs.gcePic.hasTransparency ? "YES" : "NO");
+	} else if (gf->gce.extCode == GIF_ANIM_EXT_CODE) {
+	} else {
+	}
+}
+
+/******************************************************************************/
+void gifPrintGcePicture(struct gifFile *gf);
+
+/******************************************************************************/
+void gifPrintGceAnimation(struct gifFile *gf);
+
---
+++ gifInfoDisplayer.h
@@ -0,0 +1,68 @@
+/*..--------------------------------------------------------------------------.
+../ .------------------------------------------------------------------------. \
+./´/
+|x| __--""'\
+|x|  ,__  -'""`;
+|x| /   \  /"'  \
+|x|   __// \-"-_/
+|x| ´"   \  |           > Module: gifInfoDisplayer
+|x| \     |  \  _.-"',
+|x| "^,-´\/\  '" ,--. \
+|x|  \|\| | | , /    | |
+|x|     '`'\|._ |   / /
+|x|         '\),/  / |          > Creation: July 2023
+|x|           |/.-"_/           > By: KC5-BP
+|x| .__---+-_/'|--"
+|x|         _| |_--,            > Description :
+|x|        ',/ |   /                > Functions to fully extract informations
+|x|        /|| |  /                 > of GIF Picture or Animation.
+|x|     |\| |/ |- |
+|x| .-,-/ | /  '/-"
+|x| -\|/-/\/ ^,'|
+|x| _-     |  |/
+|x|  .  --"  /
+|x| /--__,.-/
+.\`\_________________________________________________________________________/´/
+..`___________________________________________________________________________´
+===============================================================================>
+============================================================================= */
+
+#ifndef __GIFINFODISPLAYER_H__
+#define __GIFINFODISPLAYER_H__
+
+#include <stdio.h>
+#include "gifDecoder.h"
+
+#define LIGHT_CYAN "\033[1;36m"
+#define ORANGE     "\033[0;33m"
+#define GREEN      "\033[0;32m"
+#define RED        "\033[0;31m"
+#define NC         "\033[0m"
+
+#define FMT_BYTE            "%02x(%c) "
+
+#define GIF_PIC_EXT_STRING		"Graphic Control Extension"
+#define GIF_ANIM_EXT_STRING		"Application Extension"
+
+/**********************************************
+ *********************************************/
+void gifPrintSignature(char *fmt, struct gifFile *gf);
+
+/**********************************************
+ *********************************************/
+void gifPrintLogicalScreenDescriptor(struct gifFile *gf, int printGct);
+
+/**********************************************
+ *********************************************/
+void gifPrintGce(struct gifFile *gf);
+
+/**********************************************
+ *********************************************/
+void gifPrintGcePicture(struct gifFile *gf);
+
+/**********************************************
+ *********************************************/
+void gifPrintGceAnimation(struct gifFile *gf);
+
+#endif /* __GIFINFODISPLAYER_H__ */
+
