05e793c424d9d96d6c0bb901343878791f921faf
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2023-07-25 22:30:11 +0200

    Move common type, define to the upper module

--- GIFUtilities/GIFStructure/GIFGetStructure.h
+++ GIFUtilities/GIFStructure/GIFGetStructure.h
@@ -5,39 +5,6 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-/* HEADER */
-#define GIF_HEADER_SIZE 6
-#define GIF_HEADER_START_BYTE 		'G'
-
-/* LOGICAL SCREEN DESCRIPTOR (LSD) */
-#define GIF_LSD_SIZE 7
-#define GIF_LSD_WIDTH_OFFSET 		 0
-#define GIF_LSD_HEIGHT_OFFSET 		 2
-#define GIF_LSD_GCT_INFOS_OFFSET	 4
-#define GIF_LSD_BACKGROUND_OFFSET	 5
-#define GIF_LSD_TRANSPARENT_OFFSET	 6
-
-/* GRAPHICAL CONTROL EXTENSION (GCCE) */
-#define GIF_GCE_PIC_SIZE 8
-#define GIF_GCE_ANIM_SIZE 19
-#define GIF_GCE_START_BYTE 			'!'
-
-/* IMAGE DESCRIPTOR */
-#define GIF_IMG_DESCR_SIZE 10
-#define GIF_IMG_DESCR_LCT_OFFSET 	 9
-#define GIF_IMG_DESCR_START_BYTE 	','
-
-/* IMAGE DATAS */
-#define GIF_IMG_DATAS_SIZE 2
-#define GIF_IMG_DATAS_SUBBLOCK_OFFSET	1
-
-/* TRAILER */
-#define GIF_EOF ';'
-
-typedef enum {
-	GIF_PIC_EXT_CODE=0xF9, GIF_ANIM_EXT_CODE=0xFF
-} gifExtCode;
-
 struct sectionInfos {
 	fpos_t pos;
 	uint32_t subBlockSize; /* in Bytes */
--- GIFUtilities/GIFUtilities.h
+++ GIFUtilities/GIFUtilities.h
@@ -2,6 +2,40 @@
 #ifndef __GIF_UTILITIES__
 #define __GIF_UTILITIES__
 
+/* HEADER */
+#define GIF_HEADER_SIZE 6
+#define GIF_HEADER_START_BYTE 		'G'
+
+/* LOGICAL SCREEN DESCRIPTOR (LSD) */
+#define GIF_LSD_SIZE 7
+#define GIF_LSD_WIDTH_OFFSET 		 0
+#define GIF_LSD_HEIGHT_OFFSET 		 2
+#define GIF_LSD_GCT_INFOS_OFFSET	 4
+#define GIF_LSD_BACKGROUND_OFFSET	 5
+#define GIF_LSD_TRANSPARENT_OFFSET	 6
+
+/* GRAPHICAL CONTROL EXTENSION (GCCE) */
+#define GIF_GCE_PIC_SIZE 8
+#define GIF_GCE_ANIM_SIZE 19
+#define GIF_GCE_START_BYTE 			'!'
+
+/* IMAGE DESCRIPTOR */
+#define GIF_IMG_DESCR_SIZE 10
+#define GIF_IMG_DESCR_LCT_OFFSET 	 9
+#define GIF_IMG_DESCR_START_BYTE 	','
+
+/* IMAGE DATAS */
+#define GIF_IMG_DATAS_SIZE 2
+#define GIF_IMG_DATAS_SUBBLOCK_OFFSET	1
+
+/* TRAILER */
+#define GIF_EOF ';'
+
+typedef enum {
+	GIF_PIC_EXT_CODE=0xF9, GIF_ANIM_EXT_CODE=0xFF
+} gifExtCode;
+
+/* *** Sub-modules *** */
 #include "GIFStructure/GIFGetStructure.h"
 #include "GIFReader/GIFReader.h"
 
