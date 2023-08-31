ad225bc1b9a813db49947c2920742ed7170f4dce
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2023-08-31 23:18:40 +0200

    Move types defined from GIFReadContent.h + Adapt GCE structure

--- modules/giftypes.h
+++ modules/giftypes.h
@@ -41,7 +41,7 @@ typedef enum {
     GIF_PIC_EXT_CODE=0xF9, GIF_ANIM_EXT_CODE=0xFF
 } gifExtCode;
 
-/* *** STRUCTURE **************************** */
+/* *** STRUCTURE ************************************* */
 /**********************************************
  * Section's infos                            *
  ******************************************** */
@@ -101,10 +101,173 @@ struct gifStructure {
     struct sectionInfos trailer;
 };
 
-/* ****************************************** */
+/* *************************************************** */
 
-/* *** CONTENT ****************************** */
-/* ****************************************** */
+/* *** CONTENT *************************************** */
+/**********************************************
+ * Type redefinition to ease modulation
+ **********************************************/
+typedef int position_t;
+typedef unsigned char color_t;
+
+/**********************************************
+ * Dimension of a frame/picture
+ **********************************************/
+struct dimension {
+    position_t width;
+    position_t height;
+};
+
+/**********************************************
+ * Position from North-West corner's frame
+ **********************************************/
+struct location {
+    position_t x;
+    position_t y;
+};
+
+/**********************************************
+ * RGB Palette
+ **********************************************/
+struct rgb {
+    color_t r;
+    color_t g;
+    color_t b;
+};
+
+/**********************************************
+ * Logical Screen Descriptor gives infos on:
+ * - Logical Dimensions
+ * - Global Color Table (GCT)
+ * - Background index in GCT
+ * - Pixel Aspect Ratio
+ **********************************************/
+struct logicalScreenDescriptor {
+    struct dimension logicDim;
+    unsigned char gctInfos;
+    //unsigned char bitDepth : 4; /* Bit depth -1 => Largest = 7 + 1 */
+    unsigned char bitDepth;     /* Can't pass bit field as arg fn */
+    unsigned char hasGct   : 1; // TODO Check if still needed
+    unsigned char backgroundIndex;
+    unsigned char pxAspectRatio;
+};
+
+/**********************************************
+ * Color Table (CT) with number of palettes
+ * (for both Global & Local CT)
+ **********************************************/
+struct colorTable {
+    int nCol;
+    struct rgb *palette;
+};
+
+/**********************************************
+ * Specifications of a Picture's GCE
+ **********************************************/
+struct gcePicture {
+    int hasTransparency;
+    int frameDelay;     /* in hundreth of a second <=> 9 => 90[ms] */
+    int transpColNbr;   /* in GCT */
+};
+
+/**********************************************
+ * Specifications of an Animation's GCE
+ **********************************************/
+struct gceAnimation {
+    char appliName[GIF_GCE_ANIM_APPLI_NAME_SIZE+1]; /* + '\0' */
+    int nFrames; /* TODO Verified! Not number of frame at all */
+    int currentSubBlockIndex;
+    unsigned int nRepetitions;
+};
+
+/**********************************************
+ * Grouped GCE
+ **********************************************/
+union gceSpecs {
+    struct gcePicture gcePic;
+    struct gceAnimation gceAnim;
+};
+
+/**********************************************
+ * Graphical Control Extension (GCE)
+ **********************************************/
+struct gce {
+    gifExtCode extCode;
+    int nGceDatas;
+    union gceSpecs gceSpecs;
+};
+
+/**********************************************
+ *
+ **********************************************/
+struct imgDescriptor {
+    struct location pos;
+    struct dimension dim;
+    unsigned char lctInfos;
+    //unsigned char bitDepth     : 4; /* Bit depth -1 => Largest = 7 + 1 */
+    unsigned char bitDepth;         /* Can't pass bit field as arg fn */
+    unsigned char isInterlaced : 1;
+    unsigned char hasLct       : 1; // TODO Check if still needed
+};
+
+/**********************************************
+ *
+ **********************************************/
+struct imgDatas {
+    int minLzwCodeSize;
+    int rawDataSize;
+    unsigned char *rawDatas;
+};
+
+/**********************************************
+ * A Frame is composed of:
+ *      a frame's gce (picture's gce)
+ *      & an image (see structure above)
+ **********************************************/
+struct frame {
+    struct gce gce;
+    //struct img img; /* Simplifies writing => img.img.descr. */
+    struct imgDescriptor descr;
+    struct colorTable lct;
+    struct imgDatas *datas;
+};
+
+/**********************************************
+ *
+ **********************************************/
+struct animation {
+	struct gce gce;
+	unsigned int nFrames;
+	struct frame *frames;
+};
+
+/**********************************************
+ * Data is either composed of:
+ * - a simple image
+ * or
+ * - a group of images (animation GIF usage)
+ **********************************************/
+union gifComposition {
+    struct frame img;
+    struct animation anim;
+};
+
+/**********************************************
+ * A GIF File is based on the following sections:
+ * - GIF's header / signature
+ * - Logical Screen Descriptor
+ * - Global Color Table
+ * - Graphical Control Extension
+ * - Data (Picture or Animation)
+ **********************************************/
+struct gifContent {
+    char header[GIF_HEADER_SIZE+1]; /* + '\0' */
+    struct logicalScreenDescriptor lsd;
+    struct colorTable gct;
+    union gifComposition datas;
+};
+
+/* *************************************************** */
 
 #endif /* __GIF_TYPES_H__ */
 
